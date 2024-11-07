a117a287d84c26b70991bc6222dad4f02c32b949
VysotskySS <idgwynbleidd@gmail.com>
2024-11-07 19:55:26 +0700

    fix full_screen

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,8 @@ import pytest
 import allure
 import pyautogui as pag
 import time
+# import keyboard
+import subprocess
 
 # Хранит процесс приложения
 app_process = None
@@ -22,8 +24,18 @@ def run_app():
     program_name = 'rudesktop'  # Убедитесь, что это верное имя исполняемого файла
     app_process = subprocess.Popen(program_name)  # Запускаем приложение
 
+# def full_screen():
+#     pag.hotkey('alt', 'f10')
+
+
+# def full_screen():
+    # time.sleep(3)
+    # keyboard.press_and_release('alt+f10')
+
+
 def full_screen():
-    pag.hotkey('alt', 'f10')
+    time.sleep(2)
+    subprocess.Popen(['xdotool', 'key', 'alt+F10'])
 
 def close_app():
     global app_process
